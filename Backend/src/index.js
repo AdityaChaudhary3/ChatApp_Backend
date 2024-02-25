27c2b0af5ed701e3e459bd1d6bd493d54cb5f829
AdityaChaudhary3 <adityac2356@gmail.com>
2024-02-25 12:53:19 +0530

    refactor the Project structure.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,12 @@
-import express, { response } from "express"
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config({
+    path: './.env'
+})
 
 const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/api', (req, res)=>{
     const response = {hi: "here is app"}
