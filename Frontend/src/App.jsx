549bd294f0b9762328c45044793b6319fb58bdd8
AdityaChaudhary3 <adityac2356@gmail.com>
2024-03-05 00:44:08 +0530

    Create HomePage.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,26 +2,32 @@ import './App.css'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import Login from './components/Authentication/Login'
+import Signup from './components/Authentication/Signup'
+import Homepage from './Pages/Homepage'
 
 
 function App() {
 
   const [res, setRes] = useState({})
 
-  useEffect(() => {
-    axios.get('/api')
-      .then((response) => {
-        setRes(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  })
+  // useEffect(() => {
+  //   axios.get('/api')
+  //     .then((response) => {
+  //       setRes(response.data)
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //     })
+  // })
 
   return (
     <>
       <h1>Here is frontend.</h1>
-      <h2>{res.hi}</h2>
+      {/* <h2>{res.hi}</h2> */}
+      {/* <Login /> */}
+      {/* <Signup /> */}
+      <Homepage />
       <Button colorScheme='blue'>Button</Button>
     </>
   )
